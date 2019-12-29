d6538a3647de9066c48b2c4e7c6c44723f41813a
mveith <miroveith@gmail.com>
2019-12-29 21:57:35 +0100

    CORS

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 export function corsResponse(req, res, status, responseData) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://bookworm-stats.mveith.com");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if (req.method === 'OPTIONS') {
